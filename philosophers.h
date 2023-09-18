a2f6333311c05e8d4996347dde6167f401b268cf
Mehdi Poussier <mpoussie@c1r4p7.42lehavre.fr>
2023-09-18 19:32:49 +0200

    add

--- philosophers.h
+++ philosophers.h
@@ -6,18 +6,37 @@
 /*   By: mpoussie <mpoussie@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/18 15:35:36 by mpoussie          #+#    #+#             */
-/*   Updated: 2023/09/18 15:37:20 by mpoussie         ###   ########.fr       */
+/*   Updated: 2023/09/18 19:32:37 by mpoussie         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILOSOHPERS_H
-#define PHILOSOPHERS_H
+# define PHILOSOPHERS_H
 
 # include <limits.h>
+# include <pthread.h>
 # include <stdbool.h>
 # include <stddef.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <unistd.h>
+
+typedef struct s_player
+{
+}				t_player;
+
+typedef struct s_info
+{
+    int time_die;
+    int time_eat;
+    int time_sleep;
+    int running;
+}				t_info;
+
+typedef struct s_game
+{
+	t_player	player;
+	t_info		info;
+}				t_game;
 
 #endif
